25b76a05eefbe3d4a2a3db1938cbd75448a138d1
Codingfan <ma2ken.codingfan@gmail.com>
2023-05-19 07:06:13 +0900

    +page.svelteファイルでのデータ取得

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { page } from '$app/stores'
+  $: pathname = $page.url.pathname
+  $: console.log(pathname)
+</script>
+
+<nav>
+  <a href="/" class:active={pathname === '/'}>Home</a>
+  <a href="about" class:active={pathname === '/about'}>About</a>
+  <a href="/user" class:active={pathname === '/user'}>user</a>
+</nav>
+
+<slot />
+
+<style>
+  .active {
+    color: red;
+    font-weight: 900;
+  }
+</style>
---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  type User = {
+    name: string
+  }
+
+  let users: User[] = []
+
+  onMount( async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    users = data
+  } )
+</script>
+<h1>ユーザー一覧</h1>
+
+<ul>
+  {#each users as user }
+    <li>{user.name}</li>
+  {/each}
+</ul>
+
+<!-- TODO: https://reffect.co.jp/svelte/sveltekit#Active page.tsとpage.server.tsの設定  -->
