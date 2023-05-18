fab7a9ea53148f24fc69cd865aa276407b10c20c
Codingfan <ma2ken.codingfan@gmail.com>
2023-05-19 07:17:02 +0900

    +page.server.ts

---
+++ src/routes/user/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from './$types'
+
+// type User = {
+//   id: number;
+//   name: string;
+// }
+
+export const load = (async ({ fetch }) => {
+  const response = await
+fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await response.json()
+  return {
+    users
+  }
+}) satisfies PageServerLoad
